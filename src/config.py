972831a3ced69e62742ee222c323b8db184df4f6
Pavel Glavan <pglavan1998@mail.ru>
2025-07-12 16:40:27 +0300

    Add project settings

---
+++ src/config.py
@@ -0,0 +1,65 @@
+import logging
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class MainDatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="MAIN_DB_", env_file=(".env",), env_file_encoding="utf-8", extra="ignore")
+
+    host: str = Field(...)
+    port: int = Field(...)
+    user: str = Field(...)
+    password: str = Field(...)
+    database: str = Field(...)
+
+    @property
+    def connection_url(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+    
+class AnalyticDatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="ANALYTIC_DB_", env_file=(".env",), env_file_encoding="utf-8", extra="ignore")
+
+    host: str = Field(...)
+    port: int = Field(...)
+    user: str = Field(...)
+    password: str = Field(...)
+    database: str = Field(...)
+
+    @property
+    def connection_url(self) -> str:
+        return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+
+class RedisConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="REDIS_", env_file=(".env",), env_file_encoding="utf-8", extra="ignore")
+
+    host: str = Field(...)
+    port: int = Field(...)
+    username: str | None = Field(None)
+    password: str | None = Field(None)
+
+    @property
+    def connection_url(self) -> str:
+        if self.username and self.password:
+            return f"redis://{self.username}:{self.password}@{self.host}:{self.port}"
+        return f"redis://{self.host}:{self.port}"
+    
+
+class LogSettings(BaseSettings):
+    """Настройки логирования"""
+
+    base_dir: Path = Path(__file__).parent.parent
+    level: int = logging.INFO
+    log_format: str = '%(asctime)s %(levelname)s %(message)s'
+    file: Path = base_dir / "logs" / "app.log"
+
+class Settings(BaseSettings):
+    main_database: MainDatabaseSettings = MainDatabaseSettings()
+    analytic_database: AnalyticDatabaseSettings = AnalyticDatabaseSettings()
+    redis: RedisConfig = RedisConfig()
+    log: LogSettings = LogSettings()
+
+
+
+settings = Settings()
