bf5da4aa276e88518d938fe8a2b84358488db428
Pavel Glavan <pglavan1998@mail.ru>
2025-07-12 16:59:33 +0300

    Add main file in project

---
+++ src/main.py
@@ -0,0 +1,14 @@
+
+from src.connections import get_main_db_cursor, get_analytic_db_cursor, get_redis_connection
+from src.logs import logger
+
+
+def main() -> None:
+    logger.info("Начало ETL процесса.")
+    with get_main_db_cursor() as cursor, get_analytic_db_cursor() as analytic_cursor, get_redis_connection() as redis:
+        cursor.execute("SELECT 1")
+        print(cursor.fetchone())
+
+
+if __name__ == "__main__":
+    main()
