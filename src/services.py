e7362926a57f64e1b3ddb09a76ee63a2ad9c35dc
Pavel Glavan <pglavan1998@mail.ru>
2025-07-16 18:39:27 +0300

    Add changes in services file

--- src/services.py
+++ src/services.py
@@ -1,11 +1,13 @@
 import json
+from collections.abc import Generator
 from datetime import datetime
 from itertools import chain
 from pathlib import Path
-from typing import Iterator, List
+from typing import List
 
 from psycopg import Cursor, OperationalError, sql
 from psycopg.sql import Composed
+from pydantic import BaseModel, Field, ValidationError
 from redis import Redis
 
 from src.config import JOBS_DIR
@@ -13,31 +15,44 @@ from src.connections import get_analytic_db_cursor, get_main_db_cursor, get_redi
 from src.logs import logger
 
 
+class JobSchema(BaseModel):
+    create_table_query: str  = Field(validation_alias="CREATE_TABLE_SQL")
+    select_source_query: str = Field(validation_alias="SELECT_SOURCE_SQL")
+    upsert_target_query: str = Field(validation_alias="UPSERT_TARGET_SQL")
+    select_keys_query: str = Field(validation_alias="SELECT_KEYS_SQL")
+    target_table: str = Field(validation_alias="TARGET_TABLE")
+    key_columns: list[str] = Field(validation_alias="KEY_COLUMNS")
+
+
 def load_jobs() -> list[Path]:
     """Загрузка списка путей к файлам в директории jobs."""
     return [file for file in JOBS_DIR.iterdir() if file.is_file() and file.suffix == ".json"]
 
 
-def read_job(job_path: Path) -> dict[str, str | list]:
+def read_job(job_path: Path) -> JobSchema:
     """Получение задания из файла для выполнения."""
-    with open(job_path, encoding="utf-8") as f:
-        return json.load(f)
+    try:
+        with open(job_path, encoding="utf-8") as f:
+            return JobSchema(**json.load(f))
+    except ValidationError:
+        logger.error("Ошибка валидации в задании %s", job_path.name)
+        raise
 
 
-def extract_data(main_cursor: Cursor, query: str, hash_date: datetime | None,  batch_size: int = 50) -> Iterator[str]:
+def extract_data(main_cursor: Cursor, query: str, hash_date: bytes | None,  batch_size: int = 50) -> Generator[list, None, None]:
     """
     Получение данных из основной базы данных.
     :return: генератор, которые возвращает данные в "чанках"
     """
     date = datetime.min
-    if hash_date:
+    if hash_date is not None:
         date = datetime.fromisoformat(hash_date.decode('utf-8'))
     try:
         main_cursor.execute(query, [date])
     except Exception as e:
         logger.exception(e, exc_info=True)
-        raise OperationalError
-    logger.info(f'Запрос "{query}" отправлен успешно')
+        raise OperationalError from e
+    logger.info("Запрос '%s' отправлен успешно", query)
     while result := main_cursor.fetchmany(batch_size):
         yield result
 
@@ -45,9 +60,7 @@ def extract_data(main_cursor: Cursor, query: str, hash_date: datetime | None,  b
 def create_delete_query(key_columns: List[str], values_in_main_db: List[tuple], name_table: str) -> Composed:
     """Создание запроса на удаление записей из таблицы."""
 
-    query = sql.SQL("DELETE FROM {name_table}").format(
-        name_table=sql.Identifier(name_table)
-    )
+    query = sql.SQL("DELETE FROM {name_table}").format(name_table=sql.Identifier(name_table))
     if values_in_main_db:
         if len(key_columns) > 1:
             values = [
@@ -87,41 +100,35 @@ def set_redis_date(redis_key: str, redis: Redis, date_start_job: str) -> None:
 
 def process_job(job_path: Path) -> None:
     """Обработка задания."""
-    logger.info(f"Начало обработки задания: {job_path.name}")
+    logger.info("Начало обработки задания: %s", job_path.name)
     date_start_job = datetime.now().isoformat()
     job = read_job(job_path)
 
-    create_table_query: str = job["CREATE_TABLE_SQL"]
-    select_source_query: str = job["SELECT_SOURCE_SQL"]
-    upsert_target_query: str = job["UPSERT_TARGET_SQL"]
-    select_keys_query: str = job["SELECT_KEYS_SQL"]
-    target_table: str = job["TARGET_TABLE"]
-    key_columns: List[str] = job["KEY_COLUMNS"]
     try:
         with (get_main_db_cursor() as main_cursor,
               get_analytic_db_cursor() as analytic_cursor,
               get_redis_connection() as redis):
 
-            analytic_cursor.execute(create_table_query)
-            redis_key = f"{target_table}:last_datetime_job"
+            analytic_cursor.execute(job.create_table_query)
+            redis_key = f"{job.target_table}:last_datetime_job"
             hash_date = get_redis_date(redis_key, redis)
-            rows = extract_data(main_cursor, select_source_query, hash_date)
+            rows = extract_data(main_cursor, job.select_source_query, hash_date)
             data_list = []
             for rows_tuple in rows:
                 for row in rows_tuple:
                     data_list.append(row)
-            analytic_cursor.executemany(upsert_target_query, data_list)
-            logger.info(f"Вставлено/обновлено {len(data_list)} записей в таблицу {target_table}")
+            analytic_cursor.executemany(job.upsert_target_query, data_list)
+            logger.info("Вставлено/обновлено %d записей в таблицу %s", len(data_list), job.target_table)
 
-            main_cursor.execute(select_keys_query)
+            main_cursor.execute(job.select_keys_query)
             values_in_main_db = main_cursor.fetchall()
-            delete_query = create_delete_query(key_columns, values_in_main_db, target_table)
+            delete_query = create_delete_query(job.key_columns, values_in_main_db, job.target_table)
             analytic_cursor.execute(delete_query)
             deleted_count = analytic_cursor.rowcount
-            logger.info(f"Удалено {deleted_count} записей из таблицы {target_table}")
+            logger.info("Удалено %d записей из таблицы %s", deleted_count, job.target_table)
 
             analytic_cursor.connection.commit()
             set_redis_date(redis_key, redis, date_start_job)
-            logger.info(f"Задание {job_path.name} выполнено успешно")
-    except Exception as e:
-        logger.error(f"Ошибка при выполнении задания {job_path.name}: {e}")
+            logger.info("Задание %s выполнено успешно", job_path.name)
+    except Exception:
+        logger.exception("Ошибка при выполнении задания %s", job_path.name)
